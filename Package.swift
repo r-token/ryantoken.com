b92d58dd18dc2b657d5730a36a6c1603b9f1bb37
Ryan Token <ryantoken13@gmail.com>
2022-04-29 10:05:03 -0500

    Updated package.swift to use the latest

--- Package.swift
+++ Package.swift
@@ -1,15 +1,18 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.5
 
 import PackageDescription
 
 let package = Package(
     name: "RyantokenCom",
-    platforms: [.macOS("12")],
+    platforms: [.macOS(.v12)],
     products: [
         .executable(name: "RyantokenCom", targets: ["RyantokenCom"])
     ],
     dependencies: [
-        .package(url: "https://github.com/johnsundell/publish.git", from: "0.9.0")
+        .package(
+            name: "Publish",
+            url: "https://github.com/johnsundell/publish.git",
+            from: "0.9.0")
     ],
     targets: [
         .target(
